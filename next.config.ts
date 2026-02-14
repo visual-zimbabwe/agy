2032666f1972b238a17968baded391fa2424f0a3
Jean Marie Uwimana <juwimana81@gmail.com>
2026-02-14 10:14:43 -0800

    chore(next): configure allowed dev origins for localhost and 127.0.0.1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3001",
+    "http://localhost:3001",
+  ],
 };
 
 export default nextConfig;
