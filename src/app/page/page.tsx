9501e0fa75cbd59b19aa52b695c9e0460aad04d4
Jean Marie Uwimana <juwimana81@gmail.com>
2026-03-07 10:42:11 -0800

    Revert "fix: clear content on page route"
    This reverts commit 604f21a0c3ea52301547369909666ea365a0d361.

--- src/app/page/page.tsx
+++ src/app/page/page.tsx
@@ -1,3 +1,11 @@
+import { Suspense } from "react";
+
+import { PageEditor } from "@/components/page-editor/PageEditor";
+
 export default function PageRoute() {
-  return null;
+  return (
+    <Suspense fallback={null}>
+      <PageEditor />
+    </Suspense>
+  );
 }
